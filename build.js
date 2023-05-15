3ac081ddf954bff41836f7a5138600ffb272133a
utftufutukgyftryidytftuv <overby999@gmail.com>
2023-05-15 17:08:49 +0300

    15.05.23 17:08

--- build.js
+++ build.js
@@ -1,26 +1,22 @@
-import { build } from "vite";
-import dts from "vite-plugin-dts";
-import * as fs from "node:fs";
+import { build } from 'vite';
+import dts from 'vite-plugin-dts';
+import * as fs from 'node:fs';
 
-// await new Promise((resolve) => {
-//   setTimeout(resolve, 5000);
-// });
-
-fs.rmSync("./dist/types", { recursive: true, force: true });
+fs.rmSync('./dist/types', { recursive: true, force: true });
 await build({
-  build: {
-    outDir: `dist`,
-    name: "paywithcoin-sdk",
-    lib: {
-      fileName: "paywithcoin-sdk",
-      entry: ["src/paywithcoin-sdk.ts"],
-      formats: ["es", "cjs"],
-    },
-  },
-  plugins: [
-    dts({
-      tsConfigFilePath: "./tsconfig.json",
-      outputDir: "./dist/types",
-    }),
-  ],
+	build: {
+		outDir: `dist`,
+		name: 'paywithcoin-sdk',
+		lib: {
+			fileName: 'paywithcoin-sdk',
+			entry: ['src/paywithcoin-sdk.ts'],
+			formats: ['es', 'cjs']
+		}
+	},
+	plugins: [
+		dts({
+			tsConfigFilePath: './tsconfig.json',
+			outputDir: './dist/types'
+		})
+	]
 });
